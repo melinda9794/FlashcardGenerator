d4fac519bb90632f8e1f76562ac5cb799bc123c9
Melinda <melinda9794@gmail.com>
2017-02-27 19:00:56 -0500

    pushing changes

--- Flashcards.js
+++ Flashcards.js
@@ -1,4 +1,9 @@
-module.exports = {
-     BasicFlashcard: require('./BasicFlashcard.js'),
-     ClozeFlashcard: require('./ClozeFlashcard.js')
-};
+var fs = require("fs");
+var inquirer = require("inquirer");
+var right = 0;
+var wrong = 0;
+
+//Basic flashcard constructor
+function BasicFlashcard(front, back) {
+        this.front = front;
+        this.back = back;
